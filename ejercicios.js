e8b724576b00aea13faf1f8e3657d201879c650f
Leandro tombe <leandro.tombetta87@gmail.com>
2022-11-29 17:29:21 -0300

    primeros ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,82 @@
+//ejercicio 1
+
+// primeros comentarios
+
+/*primeros comentrios con
+  salto de lineas*/ 
+
+
+//ejercicio 2
+var myName;
+
+//ejercicio 3
+
+// Setup
+var a;
+
+// Only change code below this line
+
+a= 7;
+
+//ejercicio 4 - Assign the contents of a to variable b.
+
+
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b=a
+
+//ejercicio 5 - Define a variable a with var and initialize it to a value of 9.
+
+var a=9;
+
+//ejercicio 6 - Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+var myFirstName= 'Leandro'
+var myLastName= 'Tombetta'
+
+//ejercicio 7 -Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//ejercicio 8 -Modify the existing declarations and assignments so their names use camelCase.
+
+// Variable declarations
+var studlyCapVar ;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//ejercicio 9 - Update the code so it only uses the let keyword.
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// ejercicio 10 - Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//ejercicio 11 - Change the 0 so that sum will equal 20.
+
+const sum = 10 + 10;
+
+//ejercicio 12 - Change the 0 so the difference is 12.
+
+const difference = 45 - 33;
